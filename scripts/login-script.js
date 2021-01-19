c5a7a1fac6c3b31a50ca03531ae36e7fd6b03ad4
Stanley Jia <stanleyjia101@gmail.com>
2021-01-19 12:14:45 -0800

    order of initialize firebaseapp

--- scripts/login-script.js
+++ scripts/login-script.js
@@ -1,5 +1,5 @@
 // FIREBASE CONFIG FOR EUDAIMONIA
-
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyA81iaieyP5BPXTZf9V1M_xYS-sJxRPGKc",
   authDomain: "eudaimonia-f99b5.firebaseapp.com",
@@ -7,10 +7,10 @@ const firebaseConfig = {
   storageBucket: "eudaimonia-f99b5.appspot.com",
   messagingSenderId: "280495522376",
   appId: "1:280495522376:web:bf97aca351d7a867128e7d"
-};
+};*/
 
 // FIREBASE CONFIG FOR JOSEPH'S DATABASE
-/*
+
 const firebaseConfig = {
   apiKey: "AIzaSyBj7fmZRv_SCFXClGvWKN_QFr-_vxv_42w",
   authDomain: "user-auth-development.firebaseapp.com",
@@ -19,7 +19,7 @@ const firebaseConfig = {
   storageBucket: "user-auth-development.appspot.com",
   messagingSenderId: "496590033273",
   appId: "1:496590033273:web:c2f3ca11a7ddf2c6fa9fa8"
-};*/
+};
 
 
 firebase.initializeApp(firebaseConfig);
